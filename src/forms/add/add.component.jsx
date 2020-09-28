b32ba52b6b6fee2cbebdfda4459285869eebe179
Dzianis Makarenka <dzianis_makarenka@epam.com>
2020-09-28 13:23:47 +0300

    formik handleBlur

--- src/forms/add/add.component.jsx
+++ src/forms/add/add.component.jsx
@@ -4,41 +4,7 @@ import { addNewMovie } from '../../actions';
 
 import { useFormik } from 'formik';
 
-// import { validate } from '../validate';
-
-const validate = (values) => {
-    const errors = {};
-
-    if (!values.title) {
-        errors.title = 'This field is required';
-    }
-
-    if (!values.release_date) {
-        errors.release_date = 'This field is required';
-    } else if (!/^(\d){4}-(\d){1,2}-(\d){1,2}$/i.test(values.release_date)) {
-        errors.release_date = 'Invalid date format (YYYY-MM-DD)';
-    }
-
-    if (!values.url) {
-        errors.url = 'This field is required';
-    }
-
-    if (values.genres.length == 0) {
-        errors.genres = 'This field is required';
-    }
-
-    if (!values.overview) {
-        errors.overview = 'This field is required';
-    }
-
-    if (!values.runtime) {
-        errors.runtime = 'This field is required';
-    } else if (!/^(\d)+$/i.test(values.runtime)) {
-        errors.runtime = 'Please enter valid number of minutes';
-    }
-
-    return errors;
-};
+import { validate } from '../validate';
 
 const Add = ({ close }) => {
     const dispatch = useDispatch();
